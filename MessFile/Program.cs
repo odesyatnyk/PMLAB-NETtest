55e276e3ae9413e2f9981aedff4754f938f78e8a
Oleksandr Desiatnyk <korrsfeddel@gmail.com>
2020-04-11 14:30:36 +0300

    initial commit

---
+++ MessFile/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MessFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Worker("mess.txt", 10).GetCleanText();
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ MessFile/Worker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessFile
+{
+    class Worker
+    {
+        public Worker(string fileName, int limitation)
+        {
+            FileName = fileName;
+            Limitation = limitation;
+            FileContent = ReadFile();
+            FillCharEntries();
+        }
+
+        public string FileName { get; }
+        public int Limitation { get; set; }
+        public string FileContent { get; }
+        public Dictionary<char, int> CharEntries { get; } = new Dictionary<char, int>();
+
+        private string ReadFile()
+            => File.ReadAllText(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), FileName));
+
+        private void FillCharEntries()
+            => FileContent?.ToCharArray().Distinct().ToList()
+                .ForEach(x => CharEntries.Add(x, FileContent.Count(y => y == x)));
+
+        public string GetCleanText()
+        {
+            var toDelete = CharEntries.Where(x => x.Value > Limitation).Select(x => x.Key).ToArray();
+
+            return string.Join(string.Empty, FileContent.Split(toDelete));
+        }
+    }
+}
